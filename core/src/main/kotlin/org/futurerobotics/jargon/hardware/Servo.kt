76ce02112cbf50f6ee792134bdf79faaa27a31bd
Benjamin Ye <BobBenBan@users.noreply.github.com>
2019-12-07 11:58:38 -0800

    Add Servo.
    May revise later

---
+++ core/src/main/kotlin/org/futurerobotics/jargon/hardware/Servo.kt
@@ -0,0 +1,10 @@
+package org.futurerobotics.jargon.hardware
+
+/**
+ * Represents a servo. Links directly to hardware.
+ */
+interface Servo {
+
+    /** Gets/sets the position of this servo, ***in radians***. */
+    var position: Double
+}
